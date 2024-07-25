9d9142ee253a07a52461509e95ee195fa5d17f3f
unknown <2020010017388@gndu.ac.in>
2024-07-25 11:09:04 +0530

    this is commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    static void method(){
+       System.out.println("Hello Java Method");
+     }   
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");
+         method();
+    }
+}
